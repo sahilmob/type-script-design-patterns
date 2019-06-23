ac09f7793c73d1fa618edc646cb9a3f02ea667fd
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-06-23 12:19:26 +0300

    define cars classes

--- decorator.ts
+++ decorator.ts
@@ -1,2 +1,16 @@
 //Typescript decorator pattern
 
+class ModelS {
+	public description = "Model S";
+
+	public cost(): number {
+		return 73000;
+	}
+}
+class ModelX {
+	public description = "Model X";
+
+	public cost(): number {
+		return 77000;
+	}
+}

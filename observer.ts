7537980ce55b9b02786c934dcea1466b493a7770
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-06-23 13:08:51 +0300

    implement subject interface

--- observer.ts
+++ observer.ts
@@ -8,11 +8,28 @@ interface Subject {
 interface Observer {
 	update(temperature: number);
 }
-class WeatherStation {
+class WeatherStation implements Subject {
 	private temperature: number;
+	private observers: Observer[] = [];
 
 	set setTemperature(temp: number) {
 		console.log("WeatherStating: new temp. measurement: " + temp);
 		this.temperature = temp;
+		this.notifyObservers();
+	}
+
+	registerObserver(o: Observer) {
+		this.observers.push(o);
+	}
+
+	removeObserver(o: Observer) {
+		let index = this.observers.indexOf(o);
+		this.observers.splice(index, 1);
+	}
+
+	notifyObservers() {
+		for (let observer of this.observers) {
+			observer.update(this.temperature);
+		}
 	}
 }

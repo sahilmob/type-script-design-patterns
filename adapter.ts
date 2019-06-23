311ad090cad8eec553ca12aed7ea4015fe6baef1
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-06-23 14:00:43 +0300

    create and use LightningToMicroUSBAdapter

--- adapter.ts
+++ adapter.ts
@@ -17,3 +17,20 @@ class GooglePixel implements Android {
 		console.log("Using micro usb..");
 	}
 }
+
+class LightningToMicroUSBAdapter implements Android {
+	iphoneDevice: IPhone;
+
+	constructor(iphone: IPhone) {
+		this.iphoneDevice = iphone;
+	}
+
+	useMicroUSB() {
+		console.log("Want to use micro USB, converting to Lightning...");
+		this.iphoneDevice.useLightning();
+	}
+}
+
+let iphone = new iPhone7();
+let chargeAdapter = new LightningToMicroUSBAdapter(iphone);
+chargeAdapter.useMicroUSB();

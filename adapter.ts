c454181775048d479aad90b33e4632dfc4b79348
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-06-23 13:55:01 +0300

    use and implement interfaces

--- adapter.ts
+++ adapter.ts
@@ -1,10 +1,18 @@
-class iPhone7 {
+interface IPhone {
+	useLightning();
+}
+
+interface Android {
+	useMicroUSB();
+}
+
+class iPhone7 implements IPhone {
 	useLightning() {
 		console.log("Using lightning port..");
 	}
 }
 
-class GooglePixel {
+class GooglePixel implements Android {
 	useMicroUSB() {
 		console.log("Using micro usb..");
 	}

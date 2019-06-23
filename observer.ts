b857aaee8f362ea57d609e10ba143e6b00f9cf10
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-06-23 13:00:55 +0300

    init WeatherStation class

--- observer.ts
+++ observer.ts
@@ -1,1 +1,10 @@
 //Typescript observer pattern
+
+class WeatherStation {
+	private temperature: number;
+
+	set setTemperature(temp: number) {
+		console.log("WeatherStating: new temp. measurement: " + temp);
+		this.temperature = temp;
+	}
+}
